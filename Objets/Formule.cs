a29144609d34ef2a8b09f9bfddd9a2e8a2b1d314
ICyam <aandrieu@live.fr>
2020-05-06 20:12:31 +0200

    Grosse mobification du code. Transformation des fonction dans le Main et Méthode d'objet. Le programme deviens de la POO.

---
+++ Objets/Formule.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ICyamCalc.Objets
+{
+    class Formule
+    {
+        //Propriété
+        private string chaineFormule { get; set; }
+        private string uniteAngle { get; set; }
+        
+        //Constructeur
+        public Formule(string texteFormule, string angleTrigo = "rad")
+        {
+            chaineFormule = texteFormule.ToLower();
+            uniteAngle = angleTrigo;
+        }
+
+        //Methodes
+        //************************************************************************************
+
+        //Methode pour récupérer le texte de la formule
+        public string TexteFormule()
+        {
+            return this.chaineFormule;
+        }
+
+        //Fonction de calcul de formule
+        public string CalculFormule()
+        {
+            //Déclaration des varibales
+            //string formuleGauche, formuleDroite;
+            int pos;
+            string maFormule = chaineFormule;
+
+            //Traitement des parenthesess
+            //---------------------------
+            //---------------------------
+            if (NbCaracChaine('(') == NbCaracChaine(')') && NbCaracChaine('(') != 0)
+            {
+                int posParenOpen = maFormule.IndexOf('(');
+                int posParenClose = 0;
+                int testParenOpen = 1;
+                for (int i = posParenOpen + 1; i < maFormule.Length; i++)
+                {
+                    if (maFormule[i] == '(')
+                        testParenOpen++;
+                    if (maFormule[i] == ')')
+                        testParenOpen--;
+                    if (testParenOpen == 0)
+                    {
+                        posParenClose = i;
+                        break;
+                    }
+                }
+                Formule formuleG = new Formule(maFormule.Substring(0, posParenOpen));
+                Formule formuleM = new Formule(maFormule.Substring(posParenOpen + 1, posParenClose - posParenOpen - 1));
+                Formule formuleD = new Formule(maFormule.Substring(posParenClose + 1));
+                maFormule = formuleG.TexteFormule() + formuleM.CalculFormule() + formuleD.TexteFormule();
+            }
+            //Traitement de opérateurs
+            //------------------------
+            //------------------------
+
+            //Opérateurs "-"
+            pos = maFormule.IndexOf('-', 1) + 1;
+            if (pos > 1)
+            {
+                //Gére les situation du genre 2*-4 ou 7/-4 où le signe "-" suit un opérateur et ne doit être traiter comme un opérateur mathèmatique
+                string t = "0";
+                if (pos > 2)
+                    t = maFormule.Substring(pos - 2, 1);
+                if (t != "*" && t != "/" && t != "+" && t != "-" && t != "E")
+                {
+                    Formule formuleGauche = new Formule(maFormule.Substring(0, pos - 1));
+                    Formule formuleDroite = new Formule(maFormule.Substring(pos));
+                    Formule newFormule = new Formule(Convert.ToString(Convert.ToDouble(formuleGauche.CalculFormule()) - Convert.ToDouble(formuleDroite.CalculFormule())));
+                    maFormule = newFormule.CalculFormule();
+                }
+            }
+
+            //Opérateurs "+"
+            pos = maFormule.IndexOf('+') + 1;
+            if (pos > 0)
+            {
+                string t = "0";
+                if (pos > 2)
+                    t = maFormule.Substring(pos - 2, 1);
+                if (t != "E")
+                {
+                    Formule formuleGauche = new Formule(maFormule.Substring(0, pos - 1));
+                    Formule formuleDroite = new Formule(maFormule.Substring(pos));
+                    Formule newFormule = new Formule(Convert.ToString(Convert.ToDouble(formuleGauche.CalculFormule()) + Convert.ToDouble(formuleDroite.CalculFormule())));
+                    maFormule = newFormule.CalculFormule();
+                }
+            }
+
+            //Opérateurs "*"
+            pos = maFormule.IndexOf('*') + 1;
+            if (pos > 0)
+            {
+                Formule formuleGauche = new Formule(maFormule.Substring(0, pos - 1));
+                Formule formuleDroite = new Formule(maFormule.Substring(pos));
+                Formule newFormule = new Formule(Convert.ToString(Convert.ToDouble(formuleGauche.CalculFormule()) * Convert.ToDouble(formuleDroite.CalculFormule())));
+                maFormule = newFormule.CalculFormule();
+            }
+
+            //Opérateurs "/"
+            pos = maFormule.IndexOf('/') + 1;
+            if (pos > 0)
+            {
+                Formule formuleGauche = new Formule(maFormule.Substring(0, pos - 1));
+                Formule formuleDroite = new Formule(maFormule.Substring(pos));
+                Formule newFormule = new Formule(Convert.ToString(Convert.ToDouble(formuleGauche.CalculFormule()) / Convert.ToDouble(formuleDroite.CalculFormule())));
+                maFormule = newFormule.CalculFormule();
+            }
+
+            //Opérateurs "^" - Opérateur de Puissance
+            pos = maFormule.IndexOf('^') + 1;
+            if (pos > 0)
+            {
+                Formule formuleGauche = new Formule(maFormule.Substring(0, pos - 1));
+                Formule formuleDroite = new Formule(maFormule.Substring(pos));
+                Formule newFormule = new Formule(Convert.ToString(Math.Pow(Convert.ToDouble(formuleGauche.CalculFormule()), Convert.ToDouble(formuleDroite.CalculFormule()))));
+                maFormule = newFormule.CalculFormule();
+            }
+
+            //Opérateur "sqr" : racine carrée
+            if (maFormule.Length >= 4)
+            {
+                pos = maFormule.IndexOf("sqr") + 1;
+                if (pos > 0)
+                {
+                    Formule formuleDroite = new Formule(maFormule.Substring(pos + 2));
+                    Formule newFormule = new Formule(Convert.ToString(Math.Sqrt(Convert.ToDouble(formuleDroite.CalculFormule()))));
+                    maFormule = newFormule.CalculFormule();
+                }
+            }
+            return maFormule;
+        }
+
+        //Fonction qui compte le nombre ou le caractère charSeach est présent dans la chaineRef
+        public int NbCaracChaine(char charSeach)
+        {
+            int nbCarac = 0;
+            for (int i = 0; i < this.chaineFormule.Length; i++)
+            {
+                if (this.chaineFormule[i] == charSeach)
+                    nbCarac++;
+            }
+            return nbCarac;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ICyamCalc.Objets;
 
 namespace ICyamCalc
 {
@@ -13,9 +14,10 @@ namespace ICyamCalc
             do
             {
                 Console.Write(nbMem + 1 + ">");
-                string formule = Console.ReadLine();
+                Formule formule = new Formule(Console.ReadLine());
                 bool exitOk = false;
-                switch (formule)
+                string maFormule = formule.TexteFormule();
+                switch (maFormule)
                 {
                     case "cls": //Efface l'écran
                         Console.Clear();
@@ -26,12 +28,12 @@ namespace ICyamCalc
                         exitOk = true;
                         break;
                     default : //Traitement du calcul de la formule
-                        formule = formule.Replace('.', ','); //Remplace les '.' par des ','
-                        memFormule.Add(formule); //enregistrement de la formule
+                        maFormule = maFormule.Replace('.', ','); //Remplace les '.' par des ','
+                        memFormule.Add(maFormule); //enregistrement de la formule
                         nbMem ++;//incrémentation de l'index de la mémoire
                         int c = 50;
                         int l = Console.CursorTop - 1;
-                        string resultat = CalculFormule(formule); //Calcul de la formule
+                        string resultat = formule.CalculFormule(); //Calcul de la formule
                         Console.SetCursorPosition(c, l);
                         Console.ForegroundColor = ConsoleColor.Black;
                         Console.BackgroundColor = ConsoleColor.White;
@@ -43,127 +45,6 @@ namespace ICyamCalc
                     break;
             }
             while (true);
-        }
-
-        //Fonction de calcul de formule
-        static string CalculFormule(string maFormule)
-        {
-            //Déclaration des varibales
-            string formuleGauche, formuleDroite;
-            int pos;
-
-            //Traitement des parenthesess
-            //---------------------------
-            //---------------------------
-            if (NbCaracChaine('(', maFormule) == NbCaracChaine(')',maFormule) && NbCaracChaine('(',maFormule) != 0)
-            {
-                int posParenOpen = maFormule.IndexOf('(');
-                int posParenClose = 0;
-                int testParenOpen = 1;
-                for (int i = posParenOpen + 1; i < maFormule.Length; i++)
-                {
-                    if (maFormule[i] == '(')
-                        testParenOpen++;
-                    if (maFormule[i] == ')')
-                        testParenOpen--;
-                    if (testParenOpen == 0)
-                    {
-                        posParenClose = i;
-                        break;
-                    }
-                }
-                string formuleG = maFormule.Substring(0, posParenOpen);
-                string formuleM = maFormule.Substring(posParenOpen + 1, posParenClose - posParenOpen - 1);
-                string formuleD = maFormule.Substring(posParenClose + 1);
-                maFormule = formuleG + CalculFormule(formuleM) + formuleD;
-            }
-            //Traitement de opérateurs
-            //------------------------
-            //------------------------
-
-            //Opérateurs "-"
-            pos = maFormule.IndexOf('-', 1) + 1;
-            if (pos > 1)
-            {
-                //Gére les situation du genre 2*-4 ou 7/-4 où le signe "-" suit un opérateur et ne doit être traiter comme un opérateur mathèmatique
-                string t = "0";
-                if (pos>2)
-                    t = maFormule.Substring(pos-2, 1);
-                if (t != "*" && t != "/" && t != "+" && t != "-" && t != "E")
-                {
-                    formuleGauche = maFormule.Substring(0, pos - 1);
-                    formuleDroite = maFormule.Substring(pos);
-                    maFormule = CalculFormule(Convert.ToString(Convert.ToDouble(CalculFormule(formuleGauche)) - Convert.ToDouble(CalculFormule(formuleDroite))));
-                }
-            }
-
-            //Opérateurs "+"
-            pos = maFormule.IndexOf('+') + 1;
-            if (pos > 0)
-            {
-                string t = "0";
-                if (pos > 2)
-                    t = maFormule.Substring(pos - 2, 1);
-                if (t != "E")
-                {
-                    formuleGauche = maFormule.Substring(0, pos - 1);
-                    formuleDroite = maFormule.Substring(pos);
-                    //Console.WriteLine(pos + " " + formuleGauche + " " + formuleDroite);
-                    maFormule = CalculFormule(Convert.ToString(Convert.ToDouble(CalculFormule(formuleGauche)) + Convert.ToDouble(CalculFormule(formuleDroite))));
-                }
-            }
-
-            //Opérateurs "*"
-            pos = maFormule.IndexOf('*') + 1;
-            if (pos > 0)
-            {
-                formuleGauche = maFormule.Substring(0, pos - 1);
-                formuleDroite = maFormule.Substring(pos);
-                maFormule = CalculFormule(Convert.ToString(Convert.ToDouble(CalculFormule(formuleGauche)) * Convert.ToDouble(CalculFormule(formuleDroite))));
-            }
-
-            //Opérateurs "/"
-            pos = maFormule.IndexOf('/') + 1;
-            if (pos > 0)
-            {
-                formuleGauche = maFormule.Substring(0, pos - 1);
-                formuleDroite = maFormule.Substring(pos);
-                maFormule = CalculFormule(Convert.ToString(Convert.ToDouble(CalculFormule(formuleGauche)) / Convert.ToDouble(CalculFormule(formuleDroite))));
-            }
-
-            //Opérateurs "^" - Opérateur de Puissance
-            pos = maFormule.IndexOf('^') + 1;
-            if (pos > 0)
-            {
-                formuleGauche = maFormule.Substring(0, pos - 1);
-                formuleDroite = maFormule.Substring(pos);
-                maFormule = CalculFormule(Convert.ToString(Math.Pow(Convert.ToDouble(CalculFormule(formuleGauche)), Convert.ToDouble(CalculFormule(formuleDroite)))));
-            }
-
-            //Opérateur "sqr" : racine carrée
-            if (maFormule.Length >= 4)
-            {
-                pos = maFormule.IndexOf("sqr") + 1;
-                if (pos > 0)
-                {
-                    //formuleGauche = maFormule.Substring(0, pos - 1);
-                    formuleDroite = maFormule.Substring(pos + 2);
-                    maFormule = CalculFormule(Convert.ToString(Math.Sqrt(Convert.ToDouble(CalculFormule(formuleDroite)))));
-                }
-            }
-            return maFormule;
-        }
-
-        //Fonction qui compte le nombre ou le caractère charSeach est présent dans la chaineRef
-        static int NbCaracChaine(char charSeach, string chaineRef)
-        {
-            int nbCarac = 0;
-            for (int i = 0; i < chaineRef.Length; i++)
-            {
-                if (chaineRef[i] == charSeach)
-                    nbCarac++;
-            }
-            return nbCarac;
         }
 
         //Fonction de présentation de l'appication
